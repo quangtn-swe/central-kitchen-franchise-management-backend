72d6c10571a2a1a7cccae1a3b3a299cf03c3e614
Nguyễn Đình Tuấn Đạt <legendboy.vn@gmail.com>
2026-03-04 12:35:55 +0700

    Feature/dat (#59)
    * :chore: setup rule github(add .yml)
    
    * refactor: redesign database structure for better performance
    
    * chore: entity, repo, dto, mapper  from member 3
    
    * feat : add base core for Outbound and audit
    
    * fix :fix class exportNoteRequireDto
    
    * fix : fix mapper to match another entity and dto
    
    * fix mapper
    
    * fix: InventoryTransaction
    
    * feat: update inventory report
    
    * feat: update ExportNote entity
    
    * feat: Mock api for exportItem
    
    * fix test.properties
    
    * fix: hard code from OpenApiConfig
    
    * feat: Export Note Service for dev 3
    
    * feat: Export Note Service for dev3(manual select)
    
    * feat: Initial ExportNotecontroller
    
    * ci: add checkstyle into xml
    
    * fix : inventory_transactions
    
    * fix: delete datainnit

--- src/main/java/com/CocOgreen/CenFra/MS/dto/ExportNoteDto.java
+++ src/main/java/com/CocOgreen/CenFra/MS/dto/ExportNoteDto.java
@@ -5,13 +5,16 @@ import lombok.Data;
 import java.time.OffsetDateTime;
 import java.util.List;
 
+import java.math.BigDecimal;
+
+
 @Data
 public class ExportNoteDto {
     private Integer exportId;
     private String exportCode;
     private Integer storeOrderId;
-    private String storeName; // Lấy từ bảng Store qua StoreOrder
+    private String storeName;
     private String status;
     private OffsetDateTime exportDate;
-    private List<ExportItemDto> items; // Chi tiết các mặt hàng đã xuất
+    private List<ExportItemDto> items;
 }
